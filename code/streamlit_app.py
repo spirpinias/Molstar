28d9a7e4148c3e649fc2607c0b232cc411e6dee1
Maxim Kuleshov <maxim@codeocean.com>
2023-04-05 18:22:20 +0000

    Changes in 3 files

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -1,3 +1,6 @@
+import base64
+from io import StringIO
+
 import streamlit as st
 
 from molstar_streamlit_component import molstar_streamlit_component
@@ -9,23 +12,31 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+def local_url(path):
+    return f'data:@file/x-pdb;base64,{base64.b64encode(open(path, "r").read().encode("UTF-8")).decode()}'
+
 def main():
     molstar, igv = st.tabs(["Mol*", "IGV"])
     with molstar:
         molstar_streamlit_component(params={
             'url': 'https://gist.githubusercontent.com/maxim-k/4722a862190a1ec609c61185f07589e8/raw/95cd818d8c4297544ac8d98df450f6380501c648/example.pdb',
-            'format': 'pdb'})
+            'format': 'pdb'}, key="url")
+
+        molstar_streamlit_component(params={
+            'url': local_url('../data/molstar_example/example.pdb'),
+            'format': 'pdb'}, key="local")        
 
     with igv:
         igv_streamlit_component(params={
-            "genome": "hg38",
-            "locus": "chr8:127,736,588-127,739,371",
-            "tracks": [
+            'genome': 'hg38',
+            'locus': 'chr8:127,736,588-127,739,371',
+            'tracks': [
                 {
-                    "name": "HG00103",
-                    "url": "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram",
-                    "indexURL": "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram.crai",
-                    "format": "cram"
+                    'name': 'BAM',                  
+                    'url': local_url('../data/HaplotypeCaller/inputs/NA12878_wgs_20.bam'),
+                    'indexURL': local_url('../data/HaplotypeCaller/inputs/NA12878_wgs_20.bai'),
+                    'format': 'bam',
+                    'type': 'alignment'
                 }
             ]
         })        
